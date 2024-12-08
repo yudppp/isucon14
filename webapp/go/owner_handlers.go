ac75443299be8700e77103db03b8a1dbcfaccdaf
yudppp <yu.d.ppp@gmail.com>
2024-12-08 13:01:13 +0900

    fixup

--- webapp/go/owner_handlers.go
+++ webapp/go/owner_handlers.go
@@ -201,20 +201,24 @@ func ownerGetChairs(w http.ResponseWriter, r *http.Request) {
        access_token,
        model,
        is_active,
-       created_at,
-       updated_at,
+       chairs.created_at,
+       chairs.updated_at,
        IFNULL(total_distance, 0) AS total_distance,
        total_distance_updated_at
 FROM chairs
-       LEFT JOIN (SELECT chair_id,
-                          SUM(IFNULL(distance, 0)) AS total_distance,
-                          MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
-                   GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
+LEFT JOIN (
+    SELECT chair_id,
+           CAST(ROUND(SUM(IFNULL(distance, 0))) AS SIGNED) AS total_distance,
+           MAX(created_at) AS total_distance_updated_at
+    FROM (
+        SELECT chair_id,
+               created_at,
+               ABS(ST_Y(location) - LAG(ST_Y(location)) OVER (PARTITION BY chair_id ORDER BY created_at))
+               + ABS(ST_X(location) - LAG(ST_X(location)) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+        FROM chair_locations
+    ) tmp
+    GROUP BY chair_id
+) distance_table ON distance_table.chair_id = chairs.id
 WHERE owner_id = ?
 `, owner.ID); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
